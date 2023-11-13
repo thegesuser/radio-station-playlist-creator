e7ed4eee1ebf3aa6f3a0a709b87b79af9e41a334
Patrick Gesenhues <Patrick.Gesenhues@outlook.de>
2023-11-13 09:40:48 +0100

    feat: use sqlite for storing temporary values

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from os.path import exists
 import json
 import deezer
 import fileinput
@@ -6,6 +5,7 @@ import requests
 import os
 from bs4 import BeautifulSoup, PageElement, ResultSet
 from dotenv import load_dotenv
+import sqlite3
 
 load_dotenv()
 
@@ -13,26 +13,16 @@ app_Id = os.getenv('APP_ID')
 app_secret = os.getenv('APP_SECRET')
 redirect_uri = os.getenv('REDIRECT_URI')
 
-def readCache(name: str):
-    if exists(".cache.json"):
-        with open(".cache.json", "r") as json_data:
-            content = json_data.readline()
-            return json.loads(content)[name]
-    else: 
-        with open(".cache.json", "w+") as json_data:
-            json_data.write('{}')
-
-def writeCache(name: str, value: str):
-    with open(".cache.json", "r+") as json_data:
-        content = json_data.readline()
-        data = json.loads(content)
-        data[name] = value
-        json_data.write(json.dumps(data))
+con = sqlite3.connect("values.sqlite")
+cur = con.cursor()
+cur.execute("CREATE TABLE IF NOT EXISTS properties (propname VARCHAR(255) PRIMARY KEY, propval VARCHAR(255));")
 
 def readDlfNovaTracks():
     page = requests.get("https://www.deutschlandfunknova.de/playlist")
     soup = BeautifulSoup(page.content, "html.parser")
-    tracks: ResultSet[PageElement] = soup.find_all('figcaption', class_= 'playlist__title')[:50]
+    tracks: ResultSet[PageElement] = soup.find_all('figcaption', class_= 'playlist__title')[:10]
+
+    print("done parsing dlf playlist. found {} tracks".format(len(tracks)))
 
     # As radio stations repeat tracks, we only care for unique tracks
     uniqueTracks = set()
@@ -43,9 +33,9 @@ def readDlfNovaTracks():
     return uniqueTracks
 
 def getAuthToken():
-    token = readCache('token')
-    if token != None:
-        return token
+    token = cur.execute("SELECT propval FROM properties WHERE propname = 'token'").fetchone()
+    if token[0] != None:
+        return token[0]
 
     print(f"Please open https://connect.deezer.com/oauth/auth.php?app_id={app_Id}&redirect_uri={redirect_uri}&perms=basic_access,email,manage_library,offline_access")
     print("Afterwards, please paste the code into this terminal")
@@ -58,7 +48,8 @@ def getAuthToken():
         }
         authResponse = requests.get('https://connect.deezer.com/oauth/access_token.php', params=auth_params)
         new_token = json.loads(authResponse.content)['access_token']
-        writeCache('token', new_token)
+        cur.execute("INSERT INTO properties VALUES ('token', '{}')".format(new_token))
+        con.commit()
         return new_token
 
 token = getAuthToken()
@@ -71,7 +62,13 @@ for singleTrack in readDlfNovaTracks():
     if len(result) > 0:
         deezerTrackIds.add(result[0].id)
 
+playlist_id = cur.execute("SELECT propval FROM properties WHERE propname = 'playlist_id'").fetchone()
+if playlist_id == None:
+    playlist_id = client.create_playlist("Deutschlandfunk Nova Playlist")
+    cur.execute("INSERT INTO properties VALUES ('playlist_id', '{}')".format(playlist_id))
+    con.commit()
+else:
+    playlist_id = playlist_id[0]
 
-playlist_id = client.create_playlist("Deutschlandfunk Nova Playlist")
 trackIds = ','.join(map(str, deezerTrackIds))
 client.request("POST", f"playlist/{playlist_id}/tracks", songs=trackIds)
