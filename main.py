e880790bfceb7c54f3a91bed4f73af70357d1e36
Patrick Gesenhues <Patrick.Gesenhues@outlook.de>
2023-11-13 14:56:04 +0100

    refactor: style improvements

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-import json
-import deezer
 import fileinput
-import requests
+import json
 import os
+import sqlite3
+
+import deezer
+import requests
 from bs4 import BeautifulSoup, PageElement, ResultSet
 from dotenv import load_dotenv
-import sqlite3
 
 load_dotenv()
 
@@ -15,29 +16,32 @@ redirect_uri = os.getenv('REDIRECT_URI')
 
 con = sqlite3.connect("values.sqlite")
 cur = con.cursor()
-cur.execute("CREATE TABLE IF NOT EXISTS properties (propname VARCHAR(255) PRIMARY KEY, propval VARCHAR(255));")
+cur.execute("CREATE TABLE IF NOT EXISTS properties (prop_name VARCHAR(255) PRIMARY KEY, prop_val VARCHAR(255));")
 
-def readDlfNovaTracks():
+
+def read_dlf_nova_tracks():
     page = requests.get("https://www.deutschlandfunknova.de/playlist")
     soup = BeautifulSoup(page.content, "html.parser")
-    tracks: ResultSet[PageElement] = soup.find_all('figcaption', class_= 'playlist__title')[:10]
+    tracks: ResultSet[PageElement] = soup.find_all('figcaption', class_='playlist__title')[:10]
 
     print("done parsing dlf playlist. found {} tracks".format(len(tracks)))
 
     # As radio stations repeat tracks, we only care for unique tracks
-    uniqueTracks = set()
-    for singleTrack in tracks:
-        title = singleTrack.find('div', class_='title').text
-        artist = singleTrack.find('div', class_='artist').text
-        uniqueTracks.add((title, artist))
-    return uniqueTracks
+    unique_tracks = set()
+    for single_track in tracks:
+        title = single_track.find('div', class_='title').text
+        artist = single_track.find('div', class_='artist').text
+        unique_tracks.add((title, artist))
+    return unique_tracks
 
-def getAuthToken():
-    token = cur.execute("SELECT propval FROM properties WHERE propname = 'token'").fetchone()
-    if token[0] != None:
-        return token[0]
 
-    print(f"Please open https://connect.deezer.com/oauth/auth.php?app_id={app_Id}&redirect_uri={redirect_uri}&perms=basic_access,email,manage_library,offline_access")
+def get_auth_token():
+    db_token = cur.execute("SELECT prop_val FROM properties WHERE prop_name = 'token'").fetchone()
+    if db_token[0] is not None:
+        return db_token[0]
+
+    print(
+        f"Please open https://connect.deezer.com/oauth/auth.php?app_id={app_Id}&redirect_uri={redirect_uri}&perms=basic_access,email,manage_library,offline_access")
     print("Afterwards, please paste the code into this terminal")
     for line in fileinput.input():
         auth_params = {
@@ -46,24 +50,25 @@ def getAuthToken():
             "code": line.rstrip(),
             "output": "json"
         }
-        authResponse = requests.get('https://connect.deezer.com/oauth/access_token.php', params=auth_params)
-        new_token = json.loads(authResponse.content)['access_token']
+        auth_response = requests.get('https://connect.deezer.com/oauth/access_token.php', params=auth_params)
+        new_token = json.loads(auth_response.content)['access_token']
         cur.execute("INSERT INTO properties VALUES ('token', '{}')".format(new_token))
         con.commit()
         return new_token
 
-token = getAuthToken()
-client = deezer.Client(access_token=token) 
+
+token = get_auth_token()
+client = deezer.Client(access_token=token)
 
 # search for deezer track ids using the parsed results
 deezerTrackIds = set()
-for singleTrack in readDlfNovaTracks():
+for singleTrack in read_dlf_nova_tracks():
     result: deezer.PaginatedList[deezer.Track] = client.search(track=singleTrack[0], artist=singleTrack[1], strict=True)
     if len(result) > 0:
         deezerTrackIds.add(result[0].id)
 
-playlist_id = cur.execute("SELECT propval FROM properties WHERE propname = 'playlist_id'").fetchone()
-if playlist_id == None:
+playlist_id = cur.execute("SELECT prop_val FROM properties WHERE prop_name = 'playlist_id'").fetchone()
+if playlist_id is None:
     playlist_id = client.create_playlist("Deutschlandfunk Nova Playlist")
     cur.execute("INSERT INTO properties VALUES ('playlist_id', '{}')".format(playlist_id))
     con.commit()
