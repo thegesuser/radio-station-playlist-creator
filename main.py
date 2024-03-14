a9d2242598d50c591c6f039557b4b9d0f1b96077
Patrick Gesenhues <Patrick.Gesenhues@outlook.de>
2024-03-14 17:46:06 +0100

    feat: add einslive plan b playlist creator

--- main.py
+++ main.py
@@ -37,6 +37,26 @@ def read_dlf_nova_tracks():
     return unique_tracks
 
 
+def read_einslive_plan_b_tracks():
+    page = requests.get("https://www1.wdr.de/radio/1live/musik/1live-plan-b/plan-b-donnerstagssendung-100.html")
+    soup = BeautifulSoup(page.content, "html.parser")
+    tracks: ResultSet[PageElement] = soup.find_all('tr', class_='data')
+
+    print("done parsing einslive playlist. found {} rows".format(len(tracks)))
+
+    # As radio stations repeat tracks, we only care for unique tracks
+    unique_tracks = set()
+    for single_track in tracks:
+        single_row = single_track.find_all('td', class_='entry')
+        artist = single_row[0].text
+        title = single_row[1].text
+        if (artist == 'Interpret' or title == 'Titel') or (artist == '' or title == ''):
+            # filter out headers and dividers
+            continue
+        unique_tracks.add((title, artist))
+    return unique_tracks
+
+
 def comma_separated_list(p_list):
     return ",".join(map(str, p_list))
 
@@ -83,25 +103,36 @@ def delete_tracks_from_playlist(playlist_id: str, track_ids: List[str]):
             client.request("DELETE", f"playlist/{playlist_id}/tracks", songs=comma_separated_list(chunk))
 
 
+def find_deezer_track_ids(parsed_tracks: set):
+    return_value = set()
+    for single_track in parsed_tracks:
+        deezer_search_result: deezer.PaginatedList[deezer.Track] = client.search(track=single_track[0],
+                                                                                 artist=single_track[1],
+                                                                                 strict=True)
+        if len(deezer_search_result) > 0:
+            return_value.add(deezer_search_result[0].id)
+    return return_value
+
+
+def update_playlist(playlist_name: str, prop_name: str, track_ids: set):
+    playlist_id = cur.execute("SELECT prop_val FROM properties WHERE prop_name = '{}'".format(prop_name)).fetchone()
+    if playlist_id is None:
+        playlist_id = client.create_playlist(playlist_name)
+        cur.execute("INSERT INTO properties VALUES ('{}', '{}')".format(prop_name, playlist_id))
+        con.commit()
+    else:
+        playlist_id = playlist_id[0]
+        track_ids_to_delete = get_track_ids_in_playlist(playlist_id)
+        delete_tracks_from_playlist(playlist_id, track_ids_to_delete)
+    client.request("POST", f"playlist/{playlist_id}/tracks", songs=comma_separated_list(track_ids))
+
+
 token = get_auth_token()
 client = deezer.Client(access_token=token)
 
-# search for deezer track ids using the parsed results
-deezerTrackIds = set()
-for singleTrack in read_dlf_nova_tracks():
-    result: deezer.PaginatedList[deezer.Track] = client.search(track=singleTrack[0], artist=singleTrack[1], strict=True)
-    if len(result) > 0:
-        deezerTrackIds.add(result[0].id)
+# search for deezer track ids using the parsed results and update
+# dlf_nova_track_ids = find_deezer_track_ids(read_dlf_nova_tracks())
+# update_playlist("Deutschlandfunk Nova Playlist", 'dlf_nova_playlist_id', dlf_nova_track_ids)
 
-playlist_id = cur.execute("SELECT prop_val FROM properties WHERE prop_name = 'playlist_id'").fetchone()
-
-if playlist_id is None:
-    playlist_id = client.create_playlist("Deutschlandfunk Nova Playlist")
-    cur.execute("INSERT INTO properties VALUES ('playlist_id', '{}')".format(playlist_id))
-    con.commit()
-else:
-    playlist_id = playlist_id[0]
-    trackIds = get_track_ids_in_playlist(playlist_id)
-    delete_tracks_from_playlist(playlist_id, trackIds)
-
-client.request("POST", f"playlist/{playlist_id}/tracks", songs=comma_separated_list(deezerTrackIds))
+einslive_track_ids = find_deezer_track_ids(read_einslive_plan_b_tracks())
+update_playlist("1LIVE Plan B Playlist", 'einslive_plan_b_playlist_id', einslive_track_ids)
